ccdc80e6a034aa3b4d9f9982f4e3a2d30c8fb2df
Timo Schwarzer <timo@schwarzer.dev>
2023-09-29 12:01:37 +0200

    Don't disable browser when resolving hierarchy

--- pages/browse.vue
+++ pages/browse.vue
@@ -11,7 +11,7 @@
       </v-breadcrumbs>
     </div>
 
-    <kopia-object-browser :entries="entries" :disabled="entriesPending || resolvingHierarchy" @open-entry="onOpenEntry" @drag-entry="onDragEntry" />
+    <kopia-object-browser :entries="entries" :disabled="entriesPending" @open-entry="onOpenEntry" @drag-entry="onDragEntry" />
   </v-container>
 </template>
 
